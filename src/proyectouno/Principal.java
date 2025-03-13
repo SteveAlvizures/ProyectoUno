fec2204138fe6b9a4f20a6e95b3168c47341455a
Steve <ealvizsuresp5@miumg.edu.gt>
2025-03-13 00:21:26 -0600

    Se agrega clase ventana y clase ListandStack

--- src/proyectouno/Principal.java
+++ src/proyectouno/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
     public static void main(String[] args) {
         Ventana vtn = new Ventana();
         
+        System.out.println("Hola");
         vtn.setDefaultCloseOperation(Ventana.EXIT_ON_CLOSE);
         vtn.setLocationRelativeTo(null);
         vtn.setVisible(true);
